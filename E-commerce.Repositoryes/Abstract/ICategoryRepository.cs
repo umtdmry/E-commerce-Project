9c92944af1aecc6a53300333746ad60369e89ede
Umut Demiray <umut54502@ogr.duzce.edu.tr>
2020-03-01 22:54:17 +0300

    Add Entity Framework Core Repository with Category and Product Add Interface Repository with Category and Product Category and Product Repository implement Interface and add metods but null metods

---
+++ E-commerce.Repositoryes/Abstract/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using E_commerce.Entity;
+
+namespace DataAccess.Abstract
+{
+    public interface ICategoryRepository
+    {
+        Category GetById(int id);
+        Category GetOne(Expression<Func<Category, bool>> filter);
+        IQueryable<Category> GetAll(Expression<Func<Category, bool>> filter);
+
+        void Create(Category category);
+        void Delete(int id);
+        void Edit(Category category, int id);
+    }
+}
---
+++ E-commerce.Repositoryes/Concrete/EFCore/EFCoreCategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using DataAccess.Abstract;
+using E_commerce.Entity;
+
+namespace DataAccess.Concrete.EFCore
+{
+    public class EFCoreCategoryRepository : ICategoryRepository
+    {
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetOne(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Category> GetAll(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Category category, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
